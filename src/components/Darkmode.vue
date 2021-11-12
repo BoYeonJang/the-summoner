41d6ba11a53640f7d35c0e80a103943df1e91685
장보연 <uko010199@gmail.com>
2021-11-12 21:45:06 +0900

    다크모드와 터미널 vue페이지 추가

---
+++ src/components/Darkmode.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="flex justify-end items-center space-x-2">
+    <span class="text-sm text-gray-800 dark:text-gray-500">Light</span>
+    <div>
+      <input type="checkbox" name="" id="toggle" class="hidden" />
+      <label for="toggle" class="cursor-pointer">
+        <div
+          class="
+            w-9
+            h-5
+            flex
+            items-center
+            bg-gray-300
+            dark:bg-gray-600
+            rounded-full
+            p-1
+          "
+        >
+          <div
+            class="
+              toggle-dot
+              w-4
+              h-4
+              bg-white
+              rounded-full
+              shadow-md
+              translate
+              duration-300
+              ease-in-out
+            "
+          ></div>
+        </div>
+      </label>
+    </div>
+    <span class="text-sm text-gray-400 dark:text-gray-100">Dark</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Darkmode",
+  props: {},
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
---
+++ src/components/Terminal.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="flex justify-center mt-40">
+    <!-- <canvas
+      id="canvas"
+      class="bg-gray-900 w-6/12 h-1/3 shadow-3xl rounded-xl p-5"
+    ></canvas> -->
+    <div
+      class="bg-gray-900 w-6/12 shadow-3xl rounded-xl p-5"
+      style="height: 30rem"
+    >
+      <div class="output">{{ content }}</div>
+      <div class="input"><input /></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Terminal",
+  props: {
+    // msg: String,
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    start() {
+      print("ㅇㄹㅁㄴㅇㄹ");
+    },
+  },
+};
+</script>
