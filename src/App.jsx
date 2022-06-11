c13ad5c0995552e90d22f812e434b1fdadf04be0
BoYeonJang <uko010199@gmail.com>
2022-06-11 21:07:32 +0900

    Screen.jsx and bg-color

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
 import { useState } from 'react'
-import './App.css'
+// import './App.css'
+import Screen from './components/Screen'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1 class="text-3xl font-bold underline">
-          Hello world!
-        </h1>
-      </header>
+    <div className='bg-slate-300 w-screen h-screen'>
+      <Screen className='top-40' />
     </div>
   )
 }
---
+++ src/components/Screen.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function Screen() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='bg-slate-800 rounded-2xl drop-shadow-4xl text-white box-border w-96 h-96 p-4 '>
+      <h1>the</h1>
+    </div>
+  )
+}
+
+export default Screen
