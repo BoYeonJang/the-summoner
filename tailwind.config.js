9b9ffc546ebca8b63910bb8852d5dbc1b3d87826
BoYeonJang <uko010199@gmail.com>
2022-06-12 22:39:20 +0900

    add color

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,11 @@ module.exports = {
             '0 35px 35px rgba(0, 0, 0, 0.25)',
             '0 45px 65px rgba(0, 0, 0, 0.15)'
         ]
+      },
+      colors: {
+        'exit': '#ff5f57',
+        'close': '#ffbc3d',
+        'minimize': '#09c84d',
       }
     },
   },
